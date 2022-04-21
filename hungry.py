64a4563e51adfbe6847eb3157e751e62b28dcf76
jayasurya karthikeyan <jayasuryatk@icloud.com>
2022-04-21 12:18:13 +0530

    pizza changes

--- hungry.py
+++ hungry.py
@@ -2,6 +2,8 @@ name = input(' Enter your name')
 hungry = input("Are you hungry")
 if hungry == 'yes':
     print(name,'is hungry')
+    print("Go get a burger")
 else:
     print(name, 'is not hungry')
+    print("Go eat something healthy")
 
